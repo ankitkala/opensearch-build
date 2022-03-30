97a621678db5dfd83e0fcf932fffabdbbe679f25
Ankit Kala <ankikala@amazon.com>
2022-03-30 16:58:37 +0530

    Support for CCR Plugin Perf tests

---
+++ src/test_workflow/perf_test/perf_test_runner_opensearch.py
@@ -0,0 +1,48 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+import argparse
+import os
+import sys
+import logging
+import yaml
+from retry.api import retry_call
+
+from git.git_repository import GitRepository
+from manifests.bundle_manifest import BundleManifest
+from system.temporary_directory import TemporaryDirectory
+from system.working_directory import WorkingDirectory
+from test_workflow.perf_test.perf_test_cluster import PerfTestCluster
+from test_workflow.perf_test.perf_test_suite import PerfTestSuite
+from test_workflow.perf_test.perf_args import PerfArgs
+from test_workflow.perf_test.perf_test_runner import PerfTestRunner
+
+class PerfTestRunnerOpenSearch(PerfTestRunner):
+    def __init__(self, args: PerfArgs, test_manifest: BundleManifest):
+        super().__init__(args, test_manifest)
+        logging.info("Running opensearch tests")
+
+    def get_infra_repo_url(self):
+        # TODO: Remove hardcoded branch
+        return "https://github.com/ankitkala/opensearch-infra.git"
+        if "GITHUB_TOKEN" in os.environ:
+            return "https://${GITHUB_TOKEN}@github.com/opensearch-project/opensearch-infra.git"
+        return "https://github.com/opensearch-project/opensearch-infra.git"
+
+
+    def run_tests(self):
+        config = yaml.safe_load(self.args.config)
+        with TemporaryDirectory(keep=self.args.keep, chdir=True) as work_dir:
+            current_workspace = os.path.join(work_dir.name, "infra")
+            logging.info("current_workspace is " + str(current_workspace))
+            print(current_workspace)
+            # TODO: Change branch back to main
+            with GitRepository(self.get_infra_repo_url(), "endpoint", current_workspace):
+                security = "security" in self.test_manifest.components
+                with WorkingDirectory(current_workspace):
+                    with PerfTestCluster.create(self.test_manifest, config, self.args.stack, security, current_workspace, self.args.keep) as test_cluster:
+                        perf_test_suite = PerfTestSuite(self.test_manifest, test_cluster.endpoint(), security, current_workspace, self.args)
+                        retry_call(perf_test_suite.execute, tries=3, delay=60, backoff=2)
--- src/test_workflow/perf_test/perf_test_suite.py
+++ src/test_workflow/perf_test/perf_test_suite.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+import json
 from system.working_directory import WorkingDirectory
 
 
@@ -9,18 +9,26 @@ class PerfTestSuite:
     Represents a performance test suite. This class runs rally test on the deployed cluster with the provided IP.
     """
 
-    def __init__(self, bundle_manifest, endpoint, security, current_workspace, args):
+    def __init__(self, bundle_manifest, endpoint, security, current_workspace, args, owner="opensearch-devops", scenario="DEFAULT"):
+        print(current_workspace)
         self.manifest = bundle_manifest
-        self.work_dir = "mensor/"
+        self.work_dir = current_workspace + "/mensor/"
         self.endpoint = endpoint
         self.security = security
         self.current_workspace = current_workspace
         self.args = args
+        endpoint_arg = ""
+        # Pass the cluster endpoints with -t for multi-cluster usecases(e.g. cross-cluster-replication)
+        if type(self.endpoint) is dict:
+            endpoint_arg = "-t '{}'".format(json.dumps(self.endpoint))
+        else:
+            endpoint_arg = "-e {self.endpoint}"
         self.command = (
-            f"pipenv run python test_config.py -i {self.endpoint} -b {self.manifest.build.id}"
+            f"pipenv run python test_config.py {endpoint_arg} -b {self.manifest.build.id}"
             f" -a {self.manifest.build.architecture} -p {self.current_workspace}"
             f" --workload {self.args.workload} --workload-options '{self.args.workload_options}'"
             f" --warmup-iters {self.args.warmup_iters} --test-iters {self.args.test_iters}"
+            f" --scenario-type {scenario} --owner {owner}"
         )
 
     def execute(self):
